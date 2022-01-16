af7cc33344df0235690b700680b92190f4550948
aditya kumar <49594041+iamkraditya@users.noreply.github.com>
2022-01-17 00:16:27 +0530

    src code

---
+++ Constructor.java
@@ -0,0 +1,44 @@
+package package5;
+class EmpInfo{
+	int id;
+	String name;
+
+void display() {
+	System.out.println(id+" "+name);
+	}
+}
+
+class Std{
+	int id;
+	String name;
+
+	Std(int i,String n)
+	{
+	id=i;
+	name=n;
+	}
+
+	void display() {
+	System.out.println(id+" "+name);
+	}
+}
+public class Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+
+		emp1.display();
+		emp2.display();
+		
+		Std std1=new Std(5,"Aditya");
+		Std std2=new Std(8,"Kumar");
+		std1.display();
+		std2.display();
+
+
+
+	}
+
+}
